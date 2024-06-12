f13a002e695685f037cad5d7bfb04745dddd1c6c
Bohdan Pokhidnia <bogdanpoh@icloud.com>
2024-06-12 10:50:33 +0300

    Initial Commit

---
+++ MoneyYours/MoneyYoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyYoursApp.swift
+//  MoneyYours
+//
+//  Created by Bohdan Pokhidnia on 12.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoneyYoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
