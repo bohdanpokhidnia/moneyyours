b43531929ca67a7833d5fe2751e33a78227e8292
Bohdan Pokhidnia <bogdanpoh@icloud.com>
2024-11-28 21:29:47 +0200

    move create alerts to external file

--- MoneyYours/Modules/AddressesModule/AddressModule/AddressSettings/AddressSettingsFeature+AlertState.swift
+++ MoneyYours/Modules/AddressesModule/AddressModule/AddressSettings/AddressSettingsFeature+AlertState.swift
@@ -5,4 +5,48 @@
 //  Created by Bohdan Pokhidnia on 28.11.2024.
 //
 
-import Foundation
+import ComposableArchitecture
+
+extension AlertState where Action == AddressSettingsFeature.RemoveAlert {
+    static func remove(addressName: String) -> AlertState {
+        AlertState(
+            title: {
+                TextState("Do yo want remove?")
+            },
+            actions: {
+                ButtonState(role: .cancel, action: .cancel) {
+                    TextState("Cancel")
+                }
+                
+                ButtonState(role: .destructive, action: .confirm) {
+                    TextState("Confirm")
+                }
+            },
+            message: {
+                TextState(addressName)
+            }
+        )
+    }
+}
+
+extension AlertState where Action == AddressSettingsFeature.ArchiveAlert {
+    static func archive(addressName: String) -> AlertState {
+        AlertState(
+            title: {
+                TextState("Do yo want move to archive?")
+            },
+            actions: {
+                ButtonState(action: .cancel) {
+                    TextState("Cancel")
+                }
+                
+                ButtonState(action: .confirm) {
+                    TextState("Confirm")
+                }
+            },
+            message: {
+                TextState(addressName)
+            }
+        )
+    }
+}
