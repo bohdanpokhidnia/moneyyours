667f0412ca12a999b9477cfc1d8b30bd9169462f
Bohdan Pokhidnia <b.pokhidnia@ks.tv>
2025-07-16 16:40:10 +0300

    updated receipt shape and add scroll on Summary screen

--- MoneyYours/Modules/AddressesModule/AddressModule/Summary/SummaryView.swift
+++ MoneyYours/Modules/AddressesModule/AddressModule/Summary/SummaryView.swift
@@ -18,35 +18,38 @@ struct SummaryView: View {
                 configuration: .clear
             )
             
-            VStack(alignment: .leading, spacing: 8) {
-                ForEach(viewModel.communalInvoiceLists) { communalInvoiceList in
-                    SummaryRow(
-                        item: communalInvoiceList.invoice.type,
-                        title: communalInvoiceList.invoice.name,
-                        price: communalInvoiceList.price
-                    )
-                    .padding(.top, viewModel.communalInvoiceLists.first == communalInvoiceList ? 16 : 0)
-                    .padding(.horizontal, 16)
-                    .padding(.bottom, viewModel.communalInvoiceLists.last == communalInvoiceList ? 8 : 0)
+            ScrollView {
+                VStack(alignment: .leading, spacing: 8) {
+                    ForEach(viewModel.communalInvoiceLists) { communalInvoiceList in
+                        SummaryRow(
+                            item: communalInvoiceList.invoice.type,
+                            title: communalInvoiceList.invoice.name,
+                            price: communalInvoiceList.price
+                        )
+                        .padding(.top, viewModel.communalInvoiceLists.first == communalInvoiceList ? 16 : 0)
+                        .padding(.horizontal, 16)
+                        .padding(.bottom, viewModel.communalInvoiceLists.last == communalInvoiceList ? 8 : 0)
+                        
+                        Divider()
+                    }
                     
-                    Divider()
+                    HStack(spacing: 0) {
+                        Text("Sum")
+                            .fontWeight(.regular)
+                            .padding(.top, 8)
+                        
+                        Spacer()
+                        
+                        Text(viewModel.sum.formatted(.ua))
+                            .fontWeight(.bold)
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 24)
                 }
-                
-                Text("Sum")
-                    .fontWeight(.regular)
-                    .padding(.horizontal, 16)
-                    .padding(.top, 8)
-                
-                Text(viewModel.sum.formatted(.ua))
-                    .fontWeight(.bold)
-                    .padding(.horizontal, 16)
-                    .padding(.bottom, 16)
+                .background(.white)
+                .clipShape(ReceiptShapeWithWaves())
+                .padding([.top, .horizontal], 16)
             }
-            .background(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 26))
-            .padding([.top, .horizontal], 16)
-            
-            Spacer()
         }
         .ignoresSafeArea(edges: [.top])
         .background(.appBackground)
